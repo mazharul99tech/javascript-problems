6a77f73b617c965873dfc885dae4ecb493c04608
mazharul99tech <mazharul99tech@gmail.com>
2020-08-10 23:57:11 +0600

    max added

---
+++ max.js
@@ -0,0 +1,24 @@
+var business = 750;
+var minister = 756;
+var sochib = 189;
+
+var max = Math.max(business, minister, sochib);
+console.log(max);
+/**
+if(business > minister){
+    if(business > sochib){
+        console.log("business is bigger");
+    }
+    else{
+        console.log("sochib is bigger");
+    }
+}
+else{
+    if(minister >sochib){
+        console.log("minister is bigger");
+    }
+    else{
+        console.log("sochib is bigger");
+    }
+}
+ */
